0301365193f5d05476fab1293f9a3b219952bdb7
Eduardo Rombi <eduardo.rombi99@gmail.com>
2021-02-20 15:20:55 -0300

    Notifications, Course and Offers implemented

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) values ('Bootcamp HTML5', 'https://cdn.pixabay.com/photo/2018/03/12/20/09/training-3220712_1280.jpg', 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Switch-course-book-grey.svg');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-02-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-02-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2022-03-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2023-03-20T03:00:00Z', 1);
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Bem vindo ao Bootcamp !', TIMESTAMP WITH TIME ZONE '2021-02-20T18:17:00Z', false, '', 1);
